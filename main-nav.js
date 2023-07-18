f7ce81316b3a86e3381e96028cf5eec63acb135d
Matthias Ploetz <matthias_ploetz@yahoo.com>
2023-07-18 12:56:24 -0700

    initial commit

---
+++ main-nav.js
@@ -0,0 +1,42 @@
+const template = document.createElement('template')
+template.innerHTML = `
+    <style>
+        nav {
+            background-color: #333;
+            padding: 1rem;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+            display: flex;
+            gap: 2ch;
+        }
+        a,
+        a:visited{
+            color: white;
+            text-decoration: none;
+            cursor: pointer;
+        }
+    </style>
+    <nav>
+        <ul>
+            <li><a href="./">Home</a></li>
+            <li><a href="./about.html">About</a></li>
+            <li><a href="./contact.html">Contact</a></li>        
+        </ul>
+    </nav>
+`
+
+export default class MainNav extends HTMLElement{
+    constructor() {
+        super()
+
+        const templateContent = template.content
+        const clone = templateContent.cloneNode(true)
+
+        const shadowRoot = this.attachShadow({ mode: 'open'})
+        shadowRoot.appendChild(clone)
+    }
+}
+
+customElements.define('main-nav', MainNav)
---
+++ main.js
@@ -0,0 +1,1 @@
+import MainNav from "main-nav";
